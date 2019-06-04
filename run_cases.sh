9663543dadce6b841b0e4ae0aee00f35680ce02c
Antonios Inglezakis <aigglezakis@gmail.com>
2019-06-04 11:56:22 +0300

    minor fix in run_cases.sh script

--- run_cases.sh
+++ run_cases.sh
@@ -7,7 +7,7 @@ simulatorExe="./sim"
 declare -a num_nodes_cases=(50 100 200 400 800 1000 ) #  1200 1500 2000
 #declare -a percentage_malicious=( 0 1 2 4 5 8 10 15 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 55 60 70 80 85) #(0 1 2 4  5 8 10  15  20  22 23 25 30)
 declare -a percentage_malicious=( 0 5 10 15 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 40 45 50 ) #(0 1 2 4  5 8 10  15  20  22 23 25 30 70 80 85)
-declare -a overlappingUNLs=(0.9 0.91 0.95 0.99)
+declare -a overlappingUNLs=( 0.9 0.91 0.95 0.99 )
 
 outputfile="./cases_output2.csv"
 
@@ -33,6 +33,7 @@ for nncase in "${num_nodes_cases[@]}"; do
 			echo --num_nodes $nncase --mal_nodes_pC $pcmal --overlappingUNLs $ovUNLs
 			# $simulatorExe --num_nodes $nncase --num_malicious $( echo $pcmal $nncase | awk '{printf "%d\n",$1*$2/100}') >> $outputfile 
 			$simulatorExe --num_nodes $nncase --mal_nodes_pC $pcmal  --overlappingUNLs $ovUNLs >> $outputfile 
+		done
 	done
 done
 
@@ -49,5 +50,3 @@ echo "Total execution time of simulation : $TTIMEM"
 
 echo "Bye Bye!"
 exit;
-
-
